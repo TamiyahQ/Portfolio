bd094b6e7318c908ed8aa32957123f976009ff53
Tamiyah <tamiyahqazi356@gmail.com>
2024-09-09 15:12:33 -0700

    commits

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
         <div className="container mx-auto text-center">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
             <div className="p-4">
-              <img src=".\images\hero (1).png" />
+              <img src=".\images\hero (1).png" alt="hero-img"/>
             </div>
             <div className="p-10">
               <h3 className="text-2xl font-bold">About <span className="text-pink-400">Me</span></h3>
