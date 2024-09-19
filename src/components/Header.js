8ee77973ee573687243cc88e250c01defb2a32a2
TamiyahQ <165490293+TamiyahQ@users.noreply.github.com>
2024-09-19 12:19:17 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,11 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-white bg-gradient-to-r from-cyan-200 text-black p-5">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold">Portfolio</h1>
+        <div className="flex items-center space-x-4">
+           <img src="./images/logo.png" alt="Logo" className="h-10 w-10" />
+           <h1 className="text-3xl font-bold">Tamiyah Qazi</h1>
+        </div>
+
 
         <button 
           className="sm:hidden text-2xl" 
