311eadeaa0d152a0c0b9ec3ee2cd8ffabd49bfd0
Tamiyah Qazi <165490293+TamiyahQ@users.noreply.github.com>
2024-09-22 03:46:50 -0700

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,8 +2,8 @@ const Resume = () => {
     return (
       <div id="resume" className="text-center mb-12">
         <a
-          href=".\Tamiyah_Qazi.pdf" 
-          download="Tamiyah_Qazi.pdf" 
+          href="Tamiyah_Qazi_Resume.pdf" 
+          download="Tamiyah_Qazi_Resume.pdf" 
           className="bg-pink-300 text-black font-bold py-2 px-4 rounded-lg hover:bg-cyan-200 transition"
         >
           Download Resume
